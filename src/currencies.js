2b8c77cc907881ae787d60ba509107f71ec697d8
Bartek Debiec <bartekdbc@gmail.com>
2022-11-30 13:53:36 +0530

    currencies array created

---
+++ src/currencies.js
@@ -0,0 +1,7 @@
+export const currencies = [
+  { short: "EUR", name: "🇪🇺 Euro", rate: 4.7804 },
+  { short: "USD", name: "🇺🇸 Dolar amerykański", rate: 4.9 },
+  { short: "CHF", name: "🇨🇭 Frank szwajcarski", rate: 4.852 },
+  { short: "GBP", name: "🇬🇧 Funt brytyjski", rate: 5.4537 },
+  { short: "HKD", name: "🇭🇰 Dolar Hongkongu", rate: 0.6242 },
+];
