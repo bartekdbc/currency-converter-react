4140e94fc5465bff98d7b085d3b46154308032d5
Bartek Debiec <bartekdbc@gmail.com>
2022-12-12 21:07:37 +0800

    theme implemented

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        black: "#000000",
+        green: "#40916c",
+        silverTree: "#67B99A",
+    },
+    breakpoint: {
+
+    }
+};
