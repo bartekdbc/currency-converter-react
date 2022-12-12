fa5b8478a373fc5d5dd3d937af614555977907d9
Bartek Debiec <bartekdbc@gmail.com>
2022-12-12 22:38:26 +0800

    Result component updated

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledResult = styled.div`
+export const StyledResult = styled.p`
   font-size: 19px;
   word-break: break-word;
   min-height: 28.5px;
