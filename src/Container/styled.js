8f85dd47b272663904ae8c7143d98887c2e0c1b4
Bartek Debiec <bartekdbc@gmail.com>
2022-12-12 21:02:10 +0800

    theme implemented into Container component

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -4,8 +4,8 @@ export const Wrapper = styled.div`
   flex-basis: 650px;
   margin: 20px auto 0px;
   padding: 30px;
-  border: 1px solid #ffffff;
+  border: 1px solid ${({ theme }) => theme.color.white};
   border-radius: 10px;
-  background-color: white;
-  box-shadow: black 0px 0px 20px;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: ${({ theme }) => theme.color.black} 0px 0px 20px;
 `;
