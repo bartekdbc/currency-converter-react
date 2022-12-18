e6a1392038ed7ab7aac19bd6c0fed106e95ab14c
Bartek Debiec <bartekdbc@gmail.com>
2022-12-18 20:48:19 +0800

    LoadingText and ErrorText implemented/ date updated

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import { currencies } from "../currencies";
 import { Result } from "./Result";
-import { 
-  StyledForm, 
-  Header, 
-  Special, 
-  StyledFieldset, 
-  Label, 
-  LabelText, 
-  FormField, 
-  Button 
+import {
+  StyledForm,
+  Header,
+  Special,
+  StyledFieldset,
+  Label,
+  LabelText,
+  FormField,
+  Button,
+  LoadingText,
+  ErrorText,
 } from "./styled";
+import { useRatesData } from "./useRatesData";
 
 const Form = () => {
   const [amount, setAmount] = useState("");
   const [currency, setCurrency] = useState(currencies[0].short);
   const [result, setResult] = useState();
+  const ratesData = useRatesData();
+  const { date, status } = ratesData;
 
   const rate = currencies.find(({ short }) => short === currency).rate;
 
@@ -40,50 +45,61 @@ const Form = () => {
       <Header>
         Kalkulator walutowy by <Special>dbc</Special>
       </Header>
-      <StyledFieldset>
-        <p>
-          <Label>
-            <LabelText>Kwota*:</LabelText>
-            <FormField
-              value={amount}
-              type="number"
-              min="0.01"
-              step="any"
-              placeholder="PLN"
-              required
-              onChange={onInputChange}
-            />
-          </Label>
-        </p>
-        <p>
-          <Label>
-            <LabelText>Waluta*:</LabelText>
-            <FormField as="select"
-              value={currency}
-              onChange={onSelectChange}
-            >
-              {currencies.map((currency) => (
-                <option 
-                key={currency.short} 
-                value={currency.short}
-                >
-                  {currency.name}
-                </option>
-              ))}
-            </FormField>
-          </Label>
-        </p>
-        <p>
-          <Button>Przelicz</Button>
-        </p>
-        <Result result={result} />
-        <p>*- wymagane pola</p>
-        <p>
-          Kursy walut pobierane są z Narodowego Banku Polskiego.
+      {status === "error" ? (
+        <ErrorText>
+          Ups...coś poszło nie tak.😟
           <br />
-          Aktualne na dzień: <strong>2022-10-24</strong>
-        </p>
-      </StyledFieldset>
+          Sprawdź swoje połączenie z internetem. <br />
+          Jeśli Twoje połączenie jest stabilne, spróbuj ponownie później.
+        </ErrorText>
+      ) : status !== "success" ? (
+        <LoadingText>
+          Trwa ładowanie danych z Europejskiego Banku Centralnego.
+        </LoadingText>
+      ) : (
+        <StyledFieldset>
+          <p>
+            <Label>
+              <LabelText>Kwota*:</LabelText>
+              <FormField
+                value={amount}
+                type="number"
+                min="0.01"
+                step="any"
+                placeholder="PLN"
+                required
+                onChange={onInputChange}
+              />
+            </Label>
+          </p>
+          <p>
+            <Label>
+              <LabelText>Waluta*:</LabelText>
+              <FormField 
+                as="select" 
+                value={currency} 
+                onChange={onSelectChange}
+                >
+                {currencies.map((currency) => (
+                  <option key={currency.short} value={currency.short}>
+                    {currency.name}
+                  </option>
+                ))}
+              </FormField>
+            </Label>
+          </p>
+          <p>
+            <Button>Przelicz</Button>
+          </p>
+          <Result result={result} />
+          <p>*- wymagane pola</p>
+          <p>
+            Kursy walut pobierane są z Europejskiego Banku Centralnego.
+            <br />
+            Aktualne na dzień: <strong>{date}</strong>
+          </p>
+        </StyledFieldset>
+      )}
     </StyledForm>
   );
 };
